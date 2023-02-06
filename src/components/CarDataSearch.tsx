d9bcca1406cb4a69aa167680550110adbe5aa7c2
ShohamMalakes <shoam233@gmail.com>
2023-02-06 17:31:32 +0200

    .

--- src/components/CarDataSearch.tsx
+++ src/components/CarDataSearch.tsx
@@ -1,14 +1,34 @@
-import { Box, Button, Flex, Grid, Image, Input, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  ScaleFade,
+  Text,
+} from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
 import { Car } from "../types/Car";
 import { api } from "../utils/api";
 import carPlateImg from "../images/car_plate1.png";
+import { useInViewport } from "react-in-viewport";
 
 function CarDataSearch() {
   const { mutateAsync: mutateCar } = api.car.get.useMutation();
   const [carNumber, setCarNumber] = useState<string>();
   const [carData, setCarData] = useState<Car>();
-  const [error, setError] = useState();
+  // const ref = useRef();
+  // const { inViewport } = useInViewport(
+  //   ref,
+  //   { rootMargin: "-100px" },
+  //   { disconnectOnLeave: false },
+  //   {}
+  // );
   const handleSearch = async () => {
     await mutateCar(
       {
@@ -26,75 +46,187 @@ function CarDataSearch() {
   return (
     <>
       {/* <Flex direction={"column"} bg={"pink"}> */}
-      <Box
-        // bg={"blue"}
-        boxSize={"sm"}
-        display={"flex"}
-        flexDirection={"column"}
-        justifyContent={"space-around"}
-        alignItems={"center"}
-      >
-        <Input
-          w={"89.5%"}
-          h={"21.5%"}
-          min={5}
-          max={8}
-          textColor={"black"}
-          value={carNumber}
-          textAlign={"center"}
-          bgImage={carPlateImg.src}
-          fontSize={"3xl"}
-          placeholder={"12-345-67"}
-          _placeholder={{ color: "black" }}
-          fontWeight={"bold"}
-          _focus={{ bg: carPlateImg.src }}
-          _hover={{ bg: carPlateImg.src }}
-          id={"carNumber"}
-          onChange={(e) => setCarNumber(e.target.value)}
-        ></Input>
-        <Button
-          isDisabled={
-            !carNumber || carNumberLenghth! < 5 || carNumberLenghth! > 8
-          }
-          onClick={handleSearch}
+      <Flex justify={"center"}>
+        <Box
+          boxSize={"sm"}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"space-around"}
+          alignItems={"center"}
         >
-          אישור
-        </Button>
-        {(carNumberLenghth! < 5 || carNumberLenghth! > 8) && (
-          <Text>מספר רכב צריך להיות בין 5-8 תווים</Text>
-        )}
-      </Box>
-      <Grid
-        templateColumns="repeat(8, 1fr)"
-        templateRows="repeat(5,1fr)"
-        h={"100%"}
-        bg={"red"}
+          <Input
+            w={"90%"}
+            h={"21.5%"}
+            min={5}
+            max={8}
+            textColor={"black"}
+            value={carNumber}
+            textAlign={"center"}
+            bgImage={carPlateImg.src}
+            fontSize={"3xl"}
+            placeholder={"12-345-67"}
+            _placeholder={{ color: "black" }}
+            fontWeight={"bold"}
+            focusBorderColor={"none"}
+            id={"carNumber"}
+            onChange={(e) => setCarNumber(e.target.value)}
+          ></Input>
+          <Button
+            // _focus={{ bg: "none" }}
+            // _hover={{ bg: "none" }}
+            isDisabled={
+              !carNumber || carNumberLenghth! < 5 || carNumberLenghth! > 8
+            }
+            onClick={handleSearch}
+          >
+            מצא רכב
+          </Button>
+          {(carNumberLenghth! < 5 || carNumberLenghth! > 8) && (
+            <Text>מספר רכב צריך להיות בין 5-8 תווים</Text>
+          )}
+          <Button isDisabled>הוסף לרכבים שלי</Button>
+        </Box>
+      </Flex>
+      <Flex
+        // templateColumns="repeat(8, 1fr)"
+        // templateRows="repeat(5,1fr)"
+        h={"55%"}
+        w={"100%"}
+        // bg={"red"}
+        border={"2px"}
+        borderRadius={"4px"}
         dir={"rtl"}
+        justifyContent={"space-around"}
+        overflowX={"hidden"}
       >
-        <Text>{carData?.baalut}</Text>
-        <Text>{carData?.degem_cd}</Text>
-        <Text>{carData?.degem_manoa}</Text>
-        <Text>{carData?.degem_nm}</Text>
-        <Text>{carData?.horaat_rishum}</Text>
-        <Text>{carData?.kinuy_mishari}</Text>
-        <Text>{carData?.kvutzat_zihum}</Text>
-        <Text>{carData?.misgeret}</Text>
-        <Text>{carData?.mispar_rechev}</Text>
-        <Text>{carData?.moed_aliya_lakvish}</Text>
-        <Text>{carData?.ramat_eivzur_betihuty}</Text>
-        <Text>{carData?.ramat_gimur}</Text>
-        <Text>{carData?.rank}</Text>
-        <Text>{carData?.shnat_yitzur}</Text>
-        <Text>{carData?.sug_degem}</Text>
-        <Text>{carData?.sug_delek_nm}</Text>
-        <Text>{carData?.tokef_dt}</Text>
-        <Text>{carData?.tozeret_cd}</Text>
-        <Text>{carData?.tozeret_nm}</Text>
-        <Text>{carData?.tzeva_cd}</Text>
-        <Text>{carData?.tzeva_rechev}</Text>
-        <Text>{carData?.zmig_ahori}</Text>
-        <Text>{carData?.zmig_kidmi}</Text>
-      </Grid>
+        {/* <ScaleFade
+          initialScale={0.9}
+          in={inViewport}
+          whileHover={{ scale: 1.1 }}
+        > */}
+        <HStack spacing={"6em"}>
+          <Card boxSize={"md"} bg={"gray.500"} p={"5"}>
+            <Heading textAlign={"center"}>מפרט טכני ואבזור</Heading>
+            <Grid
+              p={"5"}
+              h={"100%"}
+              justifyContent={"center"}
+              textAlign={"center"}
+              templateRows="repeat(5,1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem>
+                <Text>דגם מנוע</Text>
+                <Text>{carData?.degem_manoa}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>סוג דלק</Text>
+                <Text>{carData?.sug_delek_nm}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>צמיג קדמי</Text>
+                <Text>{carData?.zmig_kidmi}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>צמיג אחורי</Text>
+                <Text>{carData?.zmig_ahori}</Text>
+              </GridItem>
+            </Grid>
+          </Card>
+          <Card boxSize={"md"} bg={"gray.500"} p={"5"}>
+            <Heading textAlign={"center"}> פרטי הרכב</Heading>
+            <Grid
+              p={"5"}
+              h={"100%"}
+              justifyContent={"center"}
+              textAlign={"center"}
+              templateRows="repeat(5,1fr)"
+              templateColumns="repeat(2, 1fr)"
+            >
+              <GridItem>
+                <Text>יצרן</Text>
+                <Text>{carData?.tozeret_nm}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>דגם</Text>
+                <Text>{carData?.kinuy_mishari}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>שנת ייצור</Text>
+                <Text>{carData?.shnat_yitzur}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>רמת גימור</Text>
+                <Text>{carData?.ramat_gimur}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>תאריך מבחן אחרון</Text>
+                <Text>{carData?.mivchan_acharon_dt}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>תוקף רישוי</Text>
+                <Text>{carData?.tokef_dt}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>בעלות</Text>
+                <Text>{carData?.baalut}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>מספר שלדה</Text>
+                <Text>{carData?.misgeret}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>מספר רכב</Text>
+                <Text>{carData?.mispar_rechev}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>קוד דגם</Text>
+                <Text>{carData?.degem_nm}</Text>
+              </GridItem>
+              <GridItem>
+                <Text>קוד משרד התחבורה</Text>
+                <Text>{carData?.tozeret_cd}</Text>
+              </GridItem>
+            </Grid>
+          </Card>
+          <Card boxSize={"md"} bg={"green"}>
+            3
+          </Card>
+        </HStack>
+        {/* </ScaleFade> */}
+
+        <GridItem>
+          <Text></Text>
+          <Text>{carData?.degem_cd}</Text>
+        </GridItem>
+        <GridItem>
+          <Text></Text>
+          <Text>{carData?.horaat_rishum}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>קבוצת זיהום</Text>
+          <Text>{carData?.kvutzat_zihum}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>{carData?.moed_aliya_lakvish}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>{carData?.ramat_eivzur_betihuty}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>{carData?.rank}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>{carData?.sug_degem}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>{carData?.tzeva_cd}</Text>
+        </GridItem>
+        <GridItem>
+          <Text>צבע הרכב</Text>
+          <Text>{carData?.tzeva_rechev}</Text>
+        </GridItem>
+      </Flex>
       {/* </Flex> */}
     </>
   );
--- src/server/api/routers/car.ts
+++ src/server/api/routers/car.ts
@@ -24,3 +24,24 @@ export const carRouter = createTRPCRouter({
     return "you can now see this secret message!";
   }),
 });
+export const carInfoRouter = createTRPCRouter({
+  get: publicProcedure
+    .input(z.object({ license_plate: z.string() }))
+    .mutation(async ({ input }) => {
+      const res = await fetch(
+        `https://data.gov.il/api/3/action/datastore_search?resource_id=142afde2-6228-49f9-8a29-9b6c3a0cbe40&q=${input.license_plate}`
+      );
+
+      const data: Res = await res.json();
+
+      console.log(data.result.records[0]);
+
+      return {
+        data: data.result.records[0],
+      };
+    }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});

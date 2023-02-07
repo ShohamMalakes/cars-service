001a43afe9e80da0ec78ce9f36f67dffba21d9cb
ShohamMalakes <shoam233@gmail.com>
2023-02-07 23:38:14 +0200

    .

--- src/server/api/routers/car.ts
+++ src/server/api/routers/car.ts
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { z } from "zod";
 import { Car, Res, Response } from "../../../types/Car";
 
@@ -11,11 +10,17 @@ export const carRouter = createTRPCRouter({
       const res = await fetch(
         `https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=${input.license_plate}`
       );
-
+      
       const data: Res = await res.json();
+      const response = await fetch(
+        "https://data.gov.il/api/3/action/datastore_search?resource_id=142afde2-6228-49f9-8a29-9b6c3a0cbe40",
+        body: JSON.parse({
+          "_id": '73127'
+        })
+        );
       return {
         data: data.result.records[0],
-      };
+        };
     }),
 
   getSecretMessage: protectedProcedure.query(() => {
@@ -27,7 +32,7 @@ export const carTechnicalInfoRouter = createTRPCRouter({
     .input(z.object({ _id: z.number() }))
     .mutation(async () => {
       const res = await fetch(
-        `https://data.gov.il/api/3/action/datastore_search?resource_id=142afde2-6228-49f9-8a29-9b6c3a0cbe40&q=73111`
+        `https://data.gov.il/api/3/action/datastore_search?resource_id=142afde2-6228-49f9-8a29-9b6c3a0cbe40&q=7115056`
       );
 
       const data: Response = await res.json();
